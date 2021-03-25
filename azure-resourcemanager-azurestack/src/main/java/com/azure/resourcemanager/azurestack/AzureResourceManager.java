1cdf5ee345fe0585cb868fb6fdd4a2488e844396
Weidong Xu <weidxu@microsoft.com>
2021-03-25 15:22:11 +0800

    complete AzureResourceManager

--- azure-resourcemanager-azurestack/src/main/java/com/azure/resourcemanager/azurestack/AzureResourceManager.java
+++ azure-resourcemanager-azurestack/src/main/java/com/azure/resourcemanager/azurestack/AzureResourceManager.java
@@ -10,8 +10,14 @@ import com.azure.core.http.policy.HttpPipelinePolicy;
 import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.util.logging.ClientLogger;
+import com.azure.resourcemanager.azurestack.appservice.AppServiceManager;
+import com.azure.resourcemanager.azurestack.commerce.UsageManager;
 import com.azure.resourcemanager.azurestack.compute.ComputeManager;
+import com.azure.resourcemanager.azurestack.databoxedge.DataBoxEdgeManager;
+import com.azure.resourcemanager.azurestack.eventhubs.EventHubsManager;
 import com.azure.resourcemanager.azurestack.iothub.IotHubManager;
+import com.azure.resourcemanager.azurestack.keyvault.KeyVaultManager;
+import com.azure.resourcemanager.azurestack.monitor.MonitorManager;
 import com.azure.resourcemanager.azurestack.network.NetworkManager;
 import com.azure.resourcemanager.azurestack.resources.ResourceManager;
 import com.azure.resourcemanager.azurestack.storage.StorageManager;
@@ -22,12 +28,6 @@ import java.util.List;
 import java.util.Objects;
 
 public class AzureResourceManager {
-
-    private final ResourceManager resourceManager;
-    private final StorageManager storageManager;
-    private final ComputeManager computeManager;
-    private final NetworkManager networkManager;
-    private final IotHubManager iotHubManager;
 
     private final String subscriptionId;
     private final String tenantId;
@@ -132,86 +132,188 @@ public class AzureResourceManager {
         }
     }
 
-    public ResourceManager resource() {
-        return resourceManager;
+    public String subscriptionId() {
+        return this.subscriptionId;
     }
 
-    public StorageManager storage() {
-        return storageManager;
+    public String tenantId() {
+        return this.tenantId;
     }
 
+    private final UsageManager usageManager;
+
+    private final ComputeManager computeManager;
+
+    private final DataBoxEdgeManager dataBoxEdgeManager;
+
+    private final IotHubManager iotHubManager;
+
+    private final EventHubsManager eventHubsManager;
+
+    private final MonitorManager monitorManager;
+
+    private final KeyVaultManager keyVaultManager;
+
+    private final NetworkManager networkManager;
+
+    private final ResourceManager resourceManager;
+
+    private final StorageManager storageManager;
+
+    private final AppServiceManager appServiceManager;
+
+    /** @return the {@link UsageManager}. */
+    public UsageManager usage() {
+        return usageManager;
+    }
+
+    /** @return the {@link ComputeManager}. */
     public ComputeManager compute() {
         return computeManager;
     }
 
+    /** @return the {@link DataBoxEdgeManager}. */
+    public DataBoxEdgeManager dataBoxEdge() {
+        return dataBoxEdgeManager;
+    }
+
+    /** @return the {@link IotHubManager}. */
+    public IotHubManager iotHub() {
+        return iotHubManager;
+    }
+
+    /** @return the {@link EventHubsManager}. */
+    public EventHubsManager eventHubs() {
+        return eventHubsManager;
+    }
+
+    /** @return the {@link MonitorManager}. */
+    public MonitorManager monitor() {
+        return monitorManager;
+    }
+
+    /** @return the {@link KeyVaultManager}. */
+    public KeyVaultManager keyVault() {
+        return keyVaultManager;
+    }
+
+    /** @return the {@link NetworkManager}. */
     public NetworkManager network() {
         return networkManager;
     }
 
-    public IotHubManager iotHub() {
-        return iotHubManager;
+    /** @return the {@link ResourceManager}. */
+    public ResourceManager resource() {
+        return resourceManager;
+    }
+
+    /** @return the {@link StorageManager}. */
+    public StorageManager storage() {
+        return storageManager;
+    }
+
+    /** @return the {@link AppServiceManager}. */
+    public AppServiceManager appService() {
+        return appServiceManager;
     }
 
     private AzureResourceManager(TokenCredential credential, AzureProfile profile, Configurable configurable) {
         Objects.requireNonNull(credential, "'credential' cannot be null.");
         Objects.requireNonNull(profile, "'profile' cannot be null.");
 
+        UsageManager.Configurable usageManagerConfigurable = UsageManager.configure();
+        ComputeManager.Configurable computeManagerConfigurable = ComputeManager.configure();
+        DataBoxEdgeManager.Configurable dataBoxEdgeManagerConfigurable = DataBoxEdgeManager.configure();
+        IotHubManager.Configurable iotHubManagerConfigurable = IotHubManager.configure();
+        EventHubsManager.Configurable eventHubsManagerConfigurable = EventHubsManager.configure();
+        MonitorManager.Configurable monitorManagerConfigurable = MonitorManager.configure();
+        KeyVaultManager.Configurable keyVaultManagerConfigurable = KeyVaultManager.configure();
+        NetworkManager.Configurable networkManagerConfigurable = NetworkManager.configure();
         ResourceManager.Configurable resourceManagerConfigurable = ResourceManager.configure();
         StorageManager.Configurable storageManagerConfigurable = StorageManager.configure();
-        ComputeManager.Configurable computeManagerConfigurable = ComputeManager.configure();
-        NetworkManager.Configurable networkManagerConfigurable = NetworkManager.configure();
-        IotHubManager.Configurable iotHubManagerConfigurable = IotHubManager.configure();
+        AppServiceManager.Configurable appServiceManagerConfigurable = AppServiceManager.configure();
 
         HttpClient httpClient = configurable.httpClient;
         if (httpClient == null) {
             httpClient = HttpClient.createDefault();
         }
         if (httpClient != null) {
+            usageManagerConfigurable.withHttpClient(httpClient);
+            computeManagerConfigurable.withHttpClient(httpClient);
+            dataBoxEdgeManagerConfigurable.withHttpClient(httpClient);
+            iotHubManagerConfigurable.withHttpClient(httpClient);
+            eventHubsManagerConfigurable.withHttpClient(httpClient);
+            monitorManagerConfigurable.withHttpClient(httpClient);
+            keyVaultManagerConfigurable.withHttpClient(httpClient);
+            networkManagerConfigurable.withHttpClient(httpClient);
             resourceManagerConfigurable.withHttpClient(httpClient);
             storageManagerConfigurable.withHttpClient(httpClient);
-            computeManagerConfigurable.withHttpClient(httpClient);
-            networkManagerConfigurable.withHttpClient(httpClient);
-            iotHubManagerConfigurable.withHttpClient(httpClient);
+            appServiceManagerConfigurable.withHttpClient(httpClient);
         }
         if (configurable.httpLogOptions != null) {
+            usageManagerConfigurable.withLogOptions(configurable.httpLogOptions);
+            computeManagerConfigurable.withLogOptions(configurable.httpLogOptions);
+            dataBoxEdgeManagerConfigurable.withLogOptions(configurable.httpLogOptions);
+            iotHubManagerConfigurable.withLogOptions(configurable.httpLogOptions);
+            eventHubsManagerConfigurable.withLogOptions(configurable.httpLogOptions);
+            monitorManagerConfigurable.withLogOptions(configurable.httpLogOptions);
+            keyVaultManagerConfigurable.withLogOptions(configurable.httpLogOptions);
+            networkManagerConfigurable.withLogOptions(configurable.httpLogOptions);
             resourceManagerConfigurable.withLogOptions(configurable.httpLogOptions);
             storageManagerConfigurable.withLogOptions(configurable.httpLogOptions);
-            computeManagerConfigurable.withLogOptions(configurable.httpLogOptions);
-            networkManagerConfigurable.withLogOptions(configurable.httpLogOptions);
-            iotHubManagerConfigurable.withLogOptions(configurable.httpLogOptions);
+            appServiceManagerConfigurable.withLogOptions(configurable.httpLogOptions);
         }
         if (configurable.retryPolicy != null) {
+            usageManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
+            computeManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
+            dataBoxEdgeManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
+            iotHubManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
+            eventHubsManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
+            monitorManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
+            keyVaultManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
+            networkManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
             resourceManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
             storageManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
-            computeManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
-            networkManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
-            iotHubManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
+            appServiceManagerConfigurable.withRetryPolicy(configurable.retryPolicy);
         }
         for (HttpPipelinePolicy policy : configurable.policies) {
+            usageManagerConfigurable.withPolicy(policy);
+            computeManagerConfigurable.withPolicy(policy);
+            dataBoxEdgeManagerConfigurable.withPolicy(policy);
+            iotHubManagerConfigurable.withPolicy(policy);
+            eventHubsManagerConfigurable.withPolicy(policy);
+            monitorManagerConfigurable.withPolicy(policy);
+            keyVaultManagerConfigurable.withPolicy(policy);
+            networkManagerConfigurable.withPolicy(policy);
             resourceManagerConfigurable.withPolicy(policy);
             storageManagerConfigurable.withPolicy(policy);
-            computeManagerConfigurable.withPolicy(policy);
-            networkManagerConfigurable.withPolicy(policy);
-            iotHubManagerConfigurable.withPolicy(policy);
+            appServiceManagerConfigurable.withPolicy(policy);
         }
         if (configurable.defaultPollInterval != null) {
+            usageManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
+            computeManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
+            dataBoxEdgeManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
+            iotHubManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
+            eventHubsManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
+            monitorManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
+            keyVaultManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
+            networkManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
             resourceManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
             storageManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
-            computeManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
-            networkManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
-            iotHubManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
+            appServiceManagerConfigurable.withDefaultPollInterval(configurable.defaultPollInterval);
         }
 
-        this.resourceManager = resourceManagerConfigurable
-                .authenticate(credential, profile);
-        this.storageManager = storageManagerConfigurable
-                .authenticate(credential, profile);
-        this.computeManager = computeManagerConfigurable
-                .authenticate(credential, profile);
-        this.networkManager = networkManagerConfigurable
-                .authenticate(credential, profile);
-        this.iotHubManager = iotHubManagerConfigurable
-                .authenticate(credential, profile);
+        this.usageManager = usageManagerConfigurable.authenticate(credential, profile);
+        this.computeManager = computeManagerConfigurable.authenticate(credential, profile);
+        this.dataBoxEdgeManager = dataBoxEdgeManagerConfigurable.authenticate(credential, profile);
+        this.iotHubManager = iotHubManagerConfigurable.authenticate(credential, profile);
+        this.eventHubsManager = eventHubsManagerConfigurable.authenticate(credential, profile);
+        this.monitorManager = monitorManagerConfigurable.authenticate(credential, profile);
+        this.keyVaultManager = keyVaultManagerConfigurable.authenticate(credential, profile);
+        this.networkManager = networkManagerConfigurable.authenticate(credential, profile);
+        this.resourceManager = resourceManagerConfigurable.authenticate(credential, profile);
+        this.storageManager = storageManagerConfigurable.authenticate(credential, profile);
+        this.appServiceManager = appServiceManagerConfigurable.authenticate(credential, profile);
 
         this.subscriptionId = profile.getSubscriptionId();
         this.tenantId = profile.getTenantId();
